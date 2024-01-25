e9ed455ce4d0b05bedc22a72666e57206a82bcb2
Namikazes <egorsitenko35@gmail.com>
2024-01-25 19:35:12 +0200

    [hw-24] Admin-categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -5,12 +5,13 @@
     <table class="table table-striped">
         <thead>
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Parent</th>
-            <th scope="col"># products</th>
+            <th scope="col" style="color: black !important">@sortablelink('id', '#')</th>
+            <th scope="col">@sortablelink('name', 'Name')</th>
+            <th scope="col">@sortablelink('parent_id', 'Parent')</th>
+            <th scope="col">@sortablelink('products_count', 'Products')</th>
             <th scope="col">Created</th>
             <th scope="col">Modified</th>
+            <th scope="col">Edit</th>
         </tr>
         </thead>
         <tbody>
@@ -25,9 +26,17 @@
                         -
                     @endif
                 </td>
-                <td>{{$category->products->count()}}</td>
+                <td>{{$category->products_count}}</td>
                 <td>{{$category->created_at}}</td>
                 <td>{{$category->updated_at}}</td>
+                <td>
+                    <form method="POST" action="{{route('admin.categories.destroy', $category)}}">
+                        @csrf
+                        @method('DELETE')
+                    <a class="btn btn-warning" href="{{route('admin.categories.edit', $category)}}"><i class="fa-solid fa-file-pen"></i></a>
+                        <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                    </form>
+                </td>
             </tr>
         @endforeach
         </tbody>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::name('admin.')->prefix('admin')->middleware(['role:admin|moderator'])->group(function() {
+Route::name('admin.')->prefix('admin')->middleware(['role:admin|moderator'])->group(function () {
     Route::get('dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard');
     Route::resource('categories', \App\Http\Controllers\Admin\CategoriesController::class)->except(['show']);
 });
